9f6175f53cc4fc3698f526e621a2d7873386844f
Lokesh Selvakumar <selvakul@tcd.ie>
2021-10-22 16:53:25 +0100

    refs initial commit of cranset and lucene

---
+++ src/main/java/irws/cranfield_indexing/CranfieldDataIndexing.java
@@ -0,0 +1,122 @@
+package irws.cranfield_indexing;
+ 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.en.EnglishAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.Field.Store;
+import org.apache.lucene.document.LongPoint;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.IndexWriterConfig.OpenMode;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+ 
+/**
+ * 
+ * CranfieldDataIndexing class is used to index all the crandfield dataset preprocessed files into lucene.
+ * 
+ * @author lokesh Selvakumar
+ *
+ */
+public class CranfieldDataIndexing 
+{
+	/**
+	 * Main method that runs upon invocation of this class
+	 * @param args
+	 */
+    public static void main(String[] args)
+    {
+        final Path cranDataSetPath = Paths.get("processedFiles");
+ 
+        try
+        {
+        	//English analyzer with the default stop words
+        	Analyzer englishAnalyzer = new EnglishAnalyzer();
+            //lucene index directory is set here
+            Directory indexDirectory = FSDirectory.open( Paths.get("luceneIndex") );
+             
+            //configuring index writer and writing docs using indexCranDataSetFiles method
+            IndexWriterConfig indexConfig = new IndexWriterConfig(englishAnalyzer);
+            indexConfig.setOpenMode(OpenMode.CREATE_OR_APPEND);
+            IndexWriter writerObject = new IndexWriter(indexDirectory, indexConfig);
+            indexCranDataSetFiles(writerObject, cranDataSetPath);
+            writerObject.close();
+        } 
+        catch (IOException e) 
+        {
+        	System.err.println("indexing initial configuration failed");
+            e.printStackTrace();
+        }
+    }
+    
+    /**
+     * cranDataFileIndex is used to index the given file in lucene with the given basic file parameters
+     * 
+     * @param indexWriterObject
+     * @param filePath
+     * @param lastModified
+     * @throws IOException
+     */
+    static void cranDataFileIndex(IndexWriter indexWriterObject, Path filePath, long lastModified) throws IOException 
+    {
+        try (InputStream stream = Files.newInputStream(filePath)) 
+        {
+            Document newDocument = new Document();
+            newDocument.add(new StringField("path", filePath.toString(), Field.Store.YES));
+            newDocument.add(new LongPoint("last_modified", lastModified));
+            newDocument.add(new TextField("file_contents", new String(Files.readAllBytes(filePath)), Store.YES));
+            indexWriterObject.updateDocument(new Term("path", filePath.toString()), newDocument);
+        }
+    }
+     
+    /**
+     * 
+     * indexCranDataSetFiles method handles the core functionality of iterating over all the files in the
+     * given directory path and indexes it in lucene.
+     * 
+     * @param writerObject
+     * @param filePath
+     * @throws IOException
+     */
+    static void indexCranDataSetFiles(final IndexWriter writerObject, Path filePath) throws IOException 
+    {
+        if (Files.isDirectory(filePath)) 
+        {
+            Files.walkFileTree(filePath, new SimpleFileVisitor<Path>() 
+            {
+                @Override
+                public FileVisitResult visitFile(Path filePath, BasicFileAttributes basicFileAttrObject) throws IOException 
+                {
+                    try
+                    {
+                        cranDataFileIndex(writerObject, filePath, basicFileAttrObject.lastModifiedTime().toMillis());
+                    } 
+                    catch (IOException ioExceptionObject) 
+                    {
+                        ioExceptionObject.printStackTrace();
+                    }
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        } 
+        else
+        {
+            cranDataFileIndex(writerObject, filePath, Files.getLastModifiedTime(filePath).toMillis());
+        }
+    }
+    
+    
+}
---
+++ src/main/java/irws/cranfield_indexing/PreprocessorCranfield.java
@@ -0,0 +1,61 @@
+package irws.cranfield_indexing;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
+/**
+ * This file is used to preprocess the given cranfield dataset and split them into individual documents
+ * 
+ * @author lokesh selvakumar
+ *
+ */
+public class PreprocessorCranfield {
+	
+	/**
+	 * This method is used to generate documents with different IDs and write the String data using printwriter
+	 * 
+	 * @param StringbuilderObject
+	 * @param IdIncrement
+	 * @param currentLine
+	 * @throws FileNotFoundException
+	 * @throws UnsupportedEncodingException
+	 */
+	public static void process_files(StringBuilder StringbuilderObject,Integer IdIncrement, String currentLine) throws FileNotFoundException, UnsupportedEncodingException {
+		File currentFile = new File("processedFiles/cranfieldDocument_"+IdIncrement+".txt");
+		PrintWriter writerObject = new PrintWriter(currentFile, "UTF-8");
+		writerObject.println(StringbuilderObject.toString());
+		writerObject.close();
+		StringbuilderObject.delete(0, StringbuilderObject.length());
+		StringbuilderObject.append(currentLine + "\n");
+	}
+	/**
+	 * Main method that starts the execution of this program
+	 * 
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {
+		String cranfieldDataSet = "cranfieldDataset/cran.all.1400" ; 
+		BufferedReader readerObject = new BufferedReader(new FileReader(new File(cranfieldDataSet)));
+		String currentLine;
+		StringBuilder StringbuilderObject = new StringBuilder();
+		int IdIncrement = 1;
+		while((currentLine = readerObject.readLine()) != null){
+			if(currentLine.startsWith(".I")){
+				if(StringbuilderObject.length()!= 0){
+					process_files(StringbuilderObject,IdIncrement,currentLine);
+					IdIncrement++;
+				}
+			}
+			else {
+			StringbuilderObject.append(currentLine + "\n");
+			}
+		}
+		process_files(StringbuilderObject,IdIncrement,currentLine);
+	}
+
+}
